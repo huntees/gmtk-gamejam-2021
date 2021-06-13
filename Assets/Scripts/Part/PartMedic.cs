35a9722f500fc6832c6a619e8e05bafb09573086
Kevin-Allain <kevallain@gmail.com>
2021-06-13 16:02:09 +0100

    First prefab for partMedic and script PartMedic.Missing the function AddMedic in the PlayerControler.

---
+++ Assets/Scripts/Part/PartMedic.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PartMedic : Part
+{
+    private int medicStock = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player");
+    }
+
+    protected override void OnCollisionEnter(Collision collision)
+    {
+        base.OnCollisionEnter(collision);
+
+        if (m_collidedObject.CompareTag("Player"))
+        {
+            // todo
+            // m_playerController.AddMedic(medicStock);
+            medicStock = 0;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        if (transform.parent == null)
+        {
+            return;
+        }
+        base.Update();
+    }
+
+    public int GetMedicStock() { return medicStock; }
+
+}
