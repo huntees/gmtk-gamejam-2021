398330836d65dde86dd027091cf06b5a959718a4
Kevin-Allain <kevallain@gmail.com>
2021-06-13 02:40:56 +0100

    First version of modified SpawnManager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -11,6 +11,9 @@ public class SpawnManager : MonoBehaviour
 
     [Header("Enemy Spawn Locations")]
     [SerializeField] private Transform[] m_spawnLocations;
+
+    // [SerializeField] private Transform[] m_spawnRectanglesEdges;
+
     private int m_randomLocationIndex;
 
     [Header("Enemy Spawn Properties")]
@@ -23,15 +26,34 @@ public class SpawnManager : MonoBehaviour
 
     private int waveCount = 0;
 
+
+    public class Pair
+    {
+        public Transform transform1;
+        public Transform transform2;
+
+        public Pair(Transform trans1, Transform trans2)
+        {
+            transform1 = trans1;
+            transform2 = trans2;
+        }
+    }
+
+    private Pair pair1, pair2, pair3, pair4;
+
     // Start is called before the first frame update
     void Start()
     {
+        pair1 = new Pair(m_spawnLocations[0], m_spawnLocations[1]);
+        pair2 = new Pair(m_spawnLocations[0], m_spawnLocations[2]);
+        pair3 = new Pair(m_spawnLocations[2], m_spawnLocations[3]);
+        pair4 = new Pair(m_spawnLocations[1], m_spawnLocations[3]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!m_isSpawning && m_enemiesLeft <= 0)
+        if (!m_isSpawning && m_enemiesLeft <= 0)
         {
             waveCount++;
             InitiateEnemySpawn(2 * waveCount, 1 * waveCount);
@@ -62,7 +84,25 @@ public class SpawnManager : MonoBehaviour
 
                 //Get any enemy and set their location
                 m_instantiatedEnemy = Instantiate(m_enemyPrefabs[m_randomEnemyIndex], transform.position, transform.rotation);
-                m_instantiatedEnemy.transform.position = m_spawnLocations[m_randomLocationIndex].position;
+                // m_instantiatedEnemy.transform.position = m_spawnLocations[m_randomLocationIndex].position;
+
+                // Dirty, but this is a game jam
+                int randSpawnEdgeIndx = Random.Range(0, 4);
+                var selecPair = (randSpawnEdgeIndx == 0) ? pair1 : (randSpawnEdgeIndx == 1) ? pair2 : (randSpawnEdgeIndx == 2) ? pair3 : pair4;
+                var baseVecPair = (selecPair.transform2.position - selecPair.transform1.position).normalized;
+                int randomizerEdgeVal = Random.Range(0, 100);
+                var posEnemy = selecPair.transform1.position + baseVecPair * randomizerEdgeVal;
+
+                // float dirRandX = Random.Range(0.0f, 100.0f); float dirRandZ = Random.Range(0.0f, 100.0f);
+                // if (dirRandX < 50.0f) { dirRandX = -1.0f; } else { dirRandX = 1.0f; }
+                // if (dirRandZ < 50.0f) { dirRandZ = -1.0f; } else { dirRandZ = 1.0f; }
+                // Debug.Log("dirRandX: " + dirRandX + ", dirRandZ: " + dirRandZ);
+                // float posX = 28.0f * dirRandX + Random.Range(0, 5) * dirRandX;
+                // float posZ = 25.0f * dirRandZ + Random.Range(0, 5) * dirRandZ;
+                // Vector3 posEnemy = new Vector3(posX, 2.3f, posZ);
+                // if (isObjectHere(posEnemy)) {  } else { }
+
+                m_instantiatedEnemy.transform.position = posEnemy;
 
                 m_instantiatedEnemy.SetActive(true);
 
@@ -84,4 +124,18 @@ public class SpawnManager : MonoBehaviour
     {
         m_enemiesLeft--;
     }
+
+    bool isObjectHere(Vector3 position)
+    {
+        Collider[] intersecting = Physics.OverlapSphere(position, 0.01f);
+        if (intersecting.Length == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
 }
