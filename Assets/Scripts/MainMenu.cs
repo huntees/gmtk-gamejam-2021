161cea2319523eb9c3ab8d5cf1c0ada13fabe123
Huntees <alainsiow@gmail.com>
2021-06-13 17:22:53 +0100

    Completed Main Menu without art stuff

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject m_mainMenuObject;
+    [SerializeField] private GameObject m_controlsObject;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowControls()
+    {
+        m_mainMenuObject.SetActive(false);
+        m_controlsObject.SetActive(true);
+    }
+
+    public void ShowMainMenu()
+    {
+        m_controlsObject.SetActive(false);
+        m_mainMenuObject.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SwitchToMainMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
